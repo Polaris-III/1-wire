a67c2ff621261ed5e85d74f91e94191aa0d17e32
Polaris-III <saheksem@yandex.ru>
2020-03-21 23:41:00 +0300

    0.11
    Непрерывная передача данных от ведомого к ведущему. Стоит добавить комментов

--- environment.v
+++ environment.v
@@ -2,20 +2,34 @@
 module env_slave;
     wire en;
     wire port;
+    reg tsm = 1;
     reg reset;
     reg clk = 1;
+    reg [9:0] cnt = 0;
     reg [7:0] data = 8'b10101010;
+    wire [7:0] mem;
     
-    master lord(en, port, clk, reset);
+    master lord(en, port, clk, reset, mem);
     
     assign port = en ? 1'bz : data[0];
     
     always #5 clk = ~clk;
     
     always@(negedge en) begin
-        data <= data >> 1;  
+        tsm <= 0;
+        cnt <= 0;
     end
-      
+    always@(posedge clk) begin
+        if(~en) begin
+            
+            end
+        else begin
+            if (~tsm) begin
+                tsm <= 1;
+                data <= data >> 1; 
+            end
+        end 
+    end  
     initial begin
         reset = 1;
         #5 reset = 0;    
--- master.v
+++ master.v
@@ -1,30 +1,47 @@
 
-module master(output reg en, inout port, input clk, reset);
-    reg [9:0] cnt;
-    reg [7:0] mem;
+module master(output reg en, inout port, input clk, reset, output reg [7:0] mem);
+    reg [9:0] cnt = 0;
+    //reg [7:0] mem = 0;
     reg init = 0;
-    reg data = 0;
-    
+    reg data = 1;
+    reg rd = 0;
     assign port = en ? data : 1'bz;
     
     always@(posedge reset) begin
+        rd <= 0;
         en <= 1;       
         cnt <= 0;
-        mem <= 0;
-        data <= 0;
+        //mem <= 0;
+        data <= 1;
+        init <= 0;
     end
     always@(posedge clk) begin
         cnt <= cnt + 1;
         if (en) begin
-            if (cnt > 50) begin
+            //rd <= 0;
+            /*if (~init) begin
+                if (cnt > 480) begin
+                    en <= 0;
+                    cnt <= 0;
+                end
+            end*/
+            if (cnt > 15) begin
                 en <= 0;
-                cnt <= 0;
             end
+            else data <= 0;
         end
-        else if(cnt > 50) begin
-            mem <= mem << 1;
-            mem[0] <= port;
-            en <= 1;
+        else begin
+            if((cnt > 30) && ~rd) begin
+                mem <= mem << 1;
+                mem[0] <= port;
+                rd <= 1;
+            end
+            if (cnt > 45) begin
+                data <= 1;
+                en <= 1;
+                cnt <= 0;
+                rd <= 0;
+            end
         end
     end
 
