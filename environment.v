a3f18c9d785f41bc8720993aad2c9636403675d5
Polaris-III <saheksem@yandex.ru>
2020-03-23 21:42:09 +0300

    0.12
    Добавлен RESET-PRESENCE. С передачей ведомому проблемы

--- environment.v
+++ environment.v
@@ -1,36 +1,61 @@
 `timescale 1 ns / 1 ps
 module env_slave;
-    wire en;
-    wire port;
-    reg tsm = 1;
-    reg reset;
+    wire en;    // Pullup emulation wire
+    wire port;  
+    
+    wire [7:0] master_mem;  // Master registry for received package
+    wire [9:0] master_cnt;  // Master counter
+    wire master_init;       // Master init flag
+    wire cycl;              // Master cycl flag
+    wire rcvd;   // Master received flag
+       
+    reg reset = 0; // Master will RESET when reset signal rise
     reg clk = 1;
-    reg [9:0] cnt = 0;
-    reg [7:0] data = 8'b10101010;
-    wire [7:0] mem;
     
-    master lord(en, port, clk, reset, mem);
+    reg [9:0] cnt = 0;                                          // Internal counter
+    reg [31:0] mem = 32'b10101010_11111111_00000000_11001100;   // Data dword for transfer
     
-    assign port = en ? 1'bz : data[0];
+    reg odata = 1;  // Data for transfer
+    reg idata = 0;  // Data for receive
+    reg pres = 0;   // Presence flag(1 when master RESET)
+    reg trsm = 0;   // Transmition flag(0 when data bit transfered)
+    
+     
+    master lord(en, port, clk, reset, master_mem, master_init, master_cnt, cycl, rcvd);
+    
+    assign port = en ? 1'bz : odata;
     
     always #5 clk = ~clk;
     
     always@(negedge en) begin
-        tsm <= 0;
         cnt <= 0;
     end
+    
+    always@(posedge en) begin
+        pres <= 0;
+        trsm <= 1;
+    end
+    
     always@(posedge clk) begin
-        if(~en) begin
-            
+        cnt <= cnt + 1;
+        if (en) begin               // Receive
+            if (cnt > 40) begin     // RESET
+                pres <= 1;
             end
-        else begin
-            if (~tsm) begin
-                tsm <= 1;
-                data <= data >> 1; 
+        end
+        else begin             // Transmit
+            if (pres) begin    // PRESENCE
+                odata <= 0;
             end
-        end 
+            else begin
+                odata <= mem[0];
+                if (trsm) mem <= mem >> 1;
+                trsm <= 0;
+            end
+        end
     end  
     initial begin
+        cnt <= 0;
         reset = 1;
         #5 reset = 0;    
     end
--- master.v
+++ master.v
@@ -1,48 +1,84 @@
 
-module master(output reg en, inout port, input clk, reset, output reg [7:0] mem);
-    reg [9:0] cnt = 0;
-    //reg [7:0] mem = 0;
-    reg init = 0;
-    reg data = 1;
-    reg rd = 0;
-    assign port = en ? data : 1'bz;
+module master(output reg en, inout port, input clk, reset, output reg [31:0] mem, output reg init, output reg [9:0] cnt, output reg cycl, output reg rcvd);
+    //output reg [9:0] cnt = 0;// Time counter
+    //reg [31:0] mem = 0;      // Registry for received package
+    //reg init = 0;            // Init flag(1 when no slave)
+    reg [7:0] command;      // Command registry for transfer
+    reg odata = 1;          // Data bit for transfer 
+    reg idata = 1;          // Data bit for receive
+    //reg rcvd = 0;           // Receive flag(1 when bit received)
+    //reg cycl = 1;           // Time package flag(1 for pullup)
+    reg pres = 1;           // Presence flag(1 when no slave)
+    
+    assign port = en ? odata : 1'bz;
     
     always@(posedge reset) begin
-        rd <= 0;
-        en <= 1;       
+        en <= 1;
         cnt <= 0;
-        //mem <= 0;
-        data <= 1;
-        init <= 0;
+        rcvd <= 0;
+        init <= 1;
+        pres <= 1;
+        idata <= 1;
+        cycl = 1;
     end
+   
     always@(posedge clk) begin
         cnt <= cnt + 1;
-        if (en) begin
-            //rd <= 0;
-            /*if (~init) begin
-                if (cnt > 480) begin
+        if (en) begin               // Transmit
+            if (cycl) begin         // Space between cycles
+                odata <= 1;
+                if (cnt > 4) begin 
+                    cycl <= 0;
+                    odata <= 0;
+                    cnt <= 0;
+                end
+            end
+            else if (init) begin     // RESET
+                odata <= 0;
+                if (cnt > 48) begin
+                    cnt <= 0;
+                    cycl <= 1;
+                    init <= 0;
+                    odata <= 1;
+                end
+            end
+            else begin              // Normal mode(Receive data bit)
+                if (cnt > 2) begin
                     en <= 0;
                     cnt <= 0;
                 end
-            end*/
-            if (cnt > 15) begin
-                en <= 0;
+                else odata <= 0;
             end
-            else data <= 0;
-        end
-        else begin
-            if((cnt > 30) && ~rd) begin
-                mem <= mem << 1;
-                mem[0] <= port;
-                rd <= 1;
+        end 
+        else begin                  // Receive
+            if (pres) begin         // PRESENCE after RESET
+                if ((cnt > 4) && ~rcvd) begin
+                    idata <= port;
+                    rcvd <= 1;
+                end
+                if (cnt > 6) begin
+                    if (idata == 0) pres <= 0;
+                    else init <= 1;
+                    cnt <= 0;
+                    en <= 1;
+                    rcvd <= 0;
+                    cycl <= 1;
+                    odata <= 1;
+                end
             end
-            if (cnt > 45) begin
-                data <= 1;
-                en <= 1;
-                cnt <= 0;
-                rd <= 0;
+            else begin              // Normal mode(Receive data bit)
+                if (cnt > 2) begin
+                    idata <= port;
+                end
+                if (cnt > 4) begin
+                    mem[0] <= idata;
+                    mem <= mem << 1;
+                    en <= 1;
+                    cnt <= 0;
+                    cycl <= 1;
+                    odata <= 1;
+                end
             end
         end
     end
-
 endmodule
