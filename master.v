e9c86f80d563bd8fd69c3f487f2075b2cb38779d
Polaris-III <saheksem@yandex.ru>
2020-03-31 22:37:19 +0300

    Update master.v
    Мастер передает две команды и получает два байта(следует сделать сброс после двух байтов т.к. по команде 44h ведомый отдает 9 байтов), нет реакции на presence.

--- master.v
+++ master.v
@@ -1,15 +1,17 @@
 
  module master(inout port, input clk, reset, output reg [7:0] FLAGBYTE, output reg idata);
 	reg [15:0] RC_DWORD = 0;			//	FLAGBYTE legend
-	reg [7:0]  CMD_BYTE = 8'b11001100;			//	7 EN
+	reg [7:0]  CMD_BYTE = 8'h44;	    //	7 EN
 	reg [31:0] COUNTER  = 0;			//	6 CYCL
     //reg [7:0]  FLAGBYTE = 8'b11111000;//	5 COMMAND
 	reg [3:0]  IN_CNT   = 0;			//	4 INIT
-	reg [2:0]  OUT_CNT  = 0;			//	3 PRESENCE
+	reg [2:0]  OUT_CNT  = 0;			//	3 <RESERVED>
 	reg odata;							//	2 RECEIVED
-	//reg idata;							//	1 TRANSMITTED
+	//reg idata;						//	1 TRANSMITTED
 										//	0 <RESERVED>
-
+    reg [2:0] CMD_SEL = 111;            //  COMMAND SELECTOR
+                                        //  111 44h
+                                        //  110 BEh
     assign port = FLAGBYTE[7] ? odata : 1'bz;
     
     always@(posedge reset) begin
@@ -17,7 +19,7 @@
 		COUNTER <= 0;
 		OUT_CNT <= 0;
 		IN_CNT <= 0;
-		odata <= 0;
+		odata <= 1;
 		idata <= 1;
     end
    
@@ -32,15 +34,8 @@
 					else begin
 						COUNTER <= 0;
 						FLAGBYTE[6] <= 0;
+						idata <= 1;
 						odata <= 0;
-					end
-				end
-				8'b1011_0000 : begin			// Cycl ended, reset
-					if (COUNTER < 48000) odata <= 0;
-					else begin
-						FLAGBYTE[6] <= 1;
-						FLAGBYTE[3] <= 1;
-						COUNTER <= 0;
 					end
 				end
 				8'b1111_1000 : begin			// Cycl after reset
@@ -57,11 +52,25 @@
 						odata <= 0;
 					end
 				end
+				8'b1100_0000 : begin            // Cycl before receiving
+				    if (COUNTER < 100) begin
+						odata <= 1;
+					end
+					else FLAGBYTE[6] <= 0;
+				end
 				8'b1110_0000 : begin            // Normal cycl before transmitting
 				    if (COUNTER < 100) begin
 						odata <= 1;
 					end
 					else FLAGBYTE[6] <= 0;
+				end
+				8'b1011_0000 : begin			// Cycl ended, reset
+					if (COUNTER < 48000) odata <= 0;
+					else begin
+						FLAGBYTE[6] <= 1;
+						FLAGBYTE[3] <= 1;
+						COUNTER <= 0;
+					end
 				end
 				8'b1010_0000 : begin			// Fall before transmitting
 					if (COUNTER < 1500) odata <= 0;
@@ -70,25 +79,6 @@
 						COUNTER <= 0;
 					end
 				end
-				8'b1010_0010 : begin			// Transmit after cycl
-					if (COUNTER < 4500) odata <= CMD_BYTE[OUT_CNT];
-					else begin
-						OUT_CNT = OUT_CNT + 1;
-						FLAGBYTE[1] <= 0;
-						FLAGBYTE[6] <= 1;
-						COUNTER <= 0;
-						if (OUT_CNT == 0) begin
-						    FLAGBYTE[5] <= 0;
-						    //FLAGBYTE[3] <= 0;
-						end
-					end
-				end
-				8'b1100_0000 : begin            // Cycl before receiving
-				    if (COUNTER < 100) begin
-						odata <= 1;
-					end
-					else FLAGBYTE[6] <= 0;
-				end
 				8'b1000_0000 : begin			// Fall before receiving
 					if (COUNTER < 1500) odata <= 0;
 					else begin
@@ -96,6 +86,26 @@
 						COUNTER <= 0;
 						FLAGBYTE[7] <= 0;
 						FLAGBYTE[2] <= 1;
+					end
+				end
+				8'b1010_0010 : begin			// Transmit after cycl
+					if (COUNTER < 4500) odata <= CMD_BYTE[OUT_CNT];
+					else begin
+						FLAGBYTE[1] <= 0;
+						FLAGBYTE[6] <= 1;
+						COUNTER <= 0;
+						OUT_CNT = OUT_CNT + 1;
+						if (OUT_CNT == 0) begin
+						    if (CMD_SEL == 3'b110) begin
+						        CMD_BYTE <= 8'h44;
+						        CMD_SEL <= 3'b111;
+						        FLAGBYTE[5] <= 0;
+						    end
+						    else begin 
+						        CMD_BYTE <= 8'hBE;
+						        CMD_SEL <= 3'b110;
+						    end
+						end
 					end
 				end
 				8'b1111_1111 : begin
@@ -109,8 +119,9 @@
 				8'b0000_0100 : begin			// Receiving		
 					if (COUNTER < 4500) idata <= port;
 					else begin
-						FLAGBYTE[2] <= 1; 
+					    FLAGBYTE[2] <= 0;
 						FLAGBYTE[6] <= 1;
+						FLAGBYTE[7] <= 1;
 						COUNTER <= 0;
 						RC_DWORD[IN_CNT] <= idata;
 						IN_CNT = IN_CNT + 1;
@@ -118,13 +129,14 @@
 					end
 				end
 				8'b0000_1100 : begin			// PRESENCE searching
-					if (COUNTER > 4000) begin
+					if (COUNTER > 6000) begin
 						FLAGBYTE[7] <= 1;
 						FLAGBYTE[6] <= 1;
-						if (~idata) begin
-						    FLAGBYTE[3] <= 0;
-						    FLAGBYTE[5] <= 1;
+						FLAGBYTE[5] <= 1;
+						if (idata) begin
+						    FLAGBYTE[4] <= 1;
 						end
+						FLAGBYTE[3] <= 0;
 						FLAGBYTE[2] <= 0;
 						COUNTER <= 0;
 					end
